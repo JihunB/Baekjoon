695d0ecbb3c2cf3a388d1f561cadeaf66aa076b1
JihunB <91535297+JihunB@users.noreply.github.com>
2023-03-06 22:49:16 +0900

    Create 1001.c

---
+++ 1001.c
@@ -0,0 +1,18 @@
+//
+//  1001.c
+//  
+//
+//  Created by 백지훈 on 2023/03/06.
+//
+
+#include "stdio.h"
+
+int main(void)  {
+    
+    int a, b;
+    scanf("%d %d", &a, &b);
+        
+    printf("%d\n", a-b);
+    return 0;
+    
+}
